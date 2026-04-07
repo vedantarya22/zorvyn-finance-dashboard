dde22207a3e90f313bbf37f3b7e8c795de20e806
Apple <apple@Apples-MacBook-Pro.local>
2026-04-07 10:18:22 +0530

    minor route fixes

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -15,11 +15,11 @@ export const getUserById = async(req,res)=>{
     try{
         const user = await User.findById(req.params.id);
         if(!user){
-            res.status(400).json({success:false,message:"User not found"});
+           return res.status(404).json({success:false,message:"User not found"});
         }
-        res.status(200).json({success:true,user});
+            res.status(200).json({success:true,user});
     }catch(err){
-        res.status(500).json({success:false,message:err.message});
+         res.status(500).json({success:false,message:err.message});
     }
 };
 
